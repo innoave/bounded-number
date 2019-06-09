8987f675e9d8cfe8563401ca005c1618e20e6071
Harald Maida <harald.maida@innoave.com>
2019-06-09 23:12:32 +0200

    write doc comments and README

---
+++ README.md
@@ -0,0 +1,42 @@
+# Bounded Number
+
+The type `Bounded number` represent a number bounded between a minimum and a maximum.
+
+Once the bounds are set the value of a bounded number can not get greater than its max bound
+neither can it get lower than its min bound.
+
+Functions that modify the value of a bound number come in two variants.
+
+1.  The normal variant clips the resulting value within the defined bounds. Functions of this
+    variant are `inc`, `dec`, `incBy`, `decBy`, `set` and `map`.
+    
+    ```elm
+    Bounded.between 0 10
+        --|> Expect.equal 0 << Bounded.value
+        |> Bounded.incBy 2
+        --|> Expect.equal 2 << Bounded.value
+        |> Bounded.incBy 9
+        --|> Expect.equal 10 << Bounded.value
+    ```
+
+2.  The trying variant explicitly states whether the result would overrun the defined bounds by
+    returning a `Maybe`. These are functions which names are prefixed with "try" like `tryInc`,
+    `tryDec`, `trySet`, `tryMap`.
+
+    ```elm
+    Bounded.between 0 10
+        --|> Expect.equal 0 << Bounded.value
+        |> Bounded.tryIncBy 2
+        --|> Expect.equal (Just 2) << Bounded.value
+        |> Bounded.tryIncBy 9
+        --|> Expect.equal Nothing << Bounded.value
+    ```
+
+The value of a bounded number can be accessed by the `value` function.
+
+```elm
+Bounded.between 0 10
+    |> Bounded.set 4
+    |> Bounded.value
+    --|> Expect.equal 4
+``` 
--- src/Number/Bounded.elm
+++ src/Number/Bounded.elm
@@ -1,23 +1,52 @@
 module Number.Bounded exposing
     ( Bounded
     , between
-    , set, trySet, map, tryMap
     , inc, dec, tryInc, tryDec, incBy, decBy, tryIncBy, tryDecBy
+    , set, trySet
+    , map, tryMap
     , value, min, max
     , decoder, encode
     )
 
 {-| A number bounded between a minimum and a maximum.
 
+Once the bounds are set the value of a bounded number can not get greater than its max bound
+neither can it get lower than its min bound.
+
+
+# Bounded numbers
+
 @docs Bounded
+
+
+# Define bounds
 
 @docs between
 
-@docs set, trySet, map, tryMap
+
+# Increment / Decrement
 
 @docs inc, dec, tryInc, tryDec, incBy, decBy, tryIncBy, tryDecBy
 
+
+# Update
+
+@docs set, trySet
+
+
+# Transform
+
+@docs map, tryMap
+
+
+# Query
+
 @docs value, min, max
+
+
+# JSON encoding / decoding
+
+@docs decoder, encode
 
 -}
 
@@ -32,7 +61,7 @@ type Bounded number
     = Bounded { min_ : number, max_ : number, value_ : number }
 
 
-{-| Construct a new bounded value by giving it a lower and a upper bound (inclusive). The value
+{-| Construct a new bounded number by giving it a lower and a upper bound (inclusive). The value
 will be initialized as the provided min. The min will always be the lower value, regardless of in
 which order you provide the arguments.
 -}
@@ -45,21 +74,76 @@ between a b =
         Bounded { min_ = b, max_ = a, value_ = b }
 
 
-decoder : JD.Decoder number -> JD.Decoder (Bounded number)
-decoder numberDecoder =
-    JD.map3 (\min_ max_ value_ -> Bounded { min_ = min_, max_ = max_, value_ = value_ })
-        (JD.field "min" numberDecoder)
-        (JD.field "max" numberDecoder)
-        (JD.field "value" numberDecoder)
+{-| Increment the bounded number by 1 and clip it to the max bound.
+
+If the resulting value is greater than the max bound the actual value is the max bound.
+
+-}
+inc : Bounded number -> Bounded number
+inc bounded =
+    incBy 1 bounded
 
 
-encode : (number -> JE.Value) -> Bounded number -> JE.Value
-encode numberEncoder (Bounded { min_, max_, value_ }) =
-    JE.object
-        [ ( "min", numberEncoder min_ )
-        , ( "max", numberEncoder max_ )
-        , ( "value", numberEncoder value_ )
-        ]
+{-| Decrement the bounded number by 1 and clip it to the min bound.
+
+If the resulting value is less than the min bound the actual value is the min bound.
+
+-}
+dec : Bounded number -> Bounded number
+dec bounded =
+    decBy 1 bounded
+
+
+{-| Try to increment the bounded number by 1. If the resulting value is greater than the max bound
+`Nothing` is returned, otherwise `Just` the result is returned.
+-}
+tryInc : Bounded number -> Maybe (Bounded number)
+tryInc bounded =
+    tryIncBy 1 bounded
+
+
+{-| Try to decrement the bounded number by 1. If the resulting value is less than the min bound
+`Nothing` is returned, otherwise `Just` the result is returned.
+-}
+tryDec : Bounded number -> Maybe (Bounded number)
+tryDec bounded =
+    tryDecBy 1 bounded
+
+
+{-| Increment the bounded number by the given number and clip it to the max bound.
+
+If the resulting value is greater than the max bound the actual value is the max bound.
+
+-}
+incBy : number -> Bounded number -> Bounded number
+incBy number bounded =
+    set (value bounded + number) bounded
+
+
+{-| Decrement the bounded number by the given number and clip it to the min bound.
+
+If the resulting value is less than the min bound the actual value is the min bound.
+
+-}
+decBy : number -> Bounded number -> Bounded number
+decBy number bounded =
+    set (value bounded - number) bounded
+
+
+{-| Try to increment the bounded number by the given number. If the resulting value is greater than
+the max bound `Nothing` is returned, otherwise `Just` the result is returned.
+-}
+tryIncBy : number -> Bounded number -> Maybe (Bounded number)
+tryIncBy number bounded =
+    trySet (value bounded + number) bounded
+
+
+{-| Try to decrement the bounded number by the given number. If the resulting value is less than the
+min bound `Nothing` is returned, otherwise `Just` the result is returned.
+-}
+tryDecBy : number -> Bounded number -> Maybe (Bounded number)
+tryDecBy number bounded =
+    trySet (value bounded - number) bounded
 
 
 {-| Set the value manually. If you try to set a value greater than the max bound, the actual value
@@ -89,46 +173,6 @@ trySet val (Bounded bounded) =
 
     else
         Nothing
-
-
-inc : Bounded number -> Bounded number
-inc bounded =
-    incBy 1 bounded
-
-
-dec : Bounded number -> Bounded number
-dec bounded =
-    decBy 1 bounded
-
-
-tryInc : Bounded number -> Maybe (Bounded number)
-tryInc bounded =
-    tryIncBy 1 bounded
-
-
-tryDec : Bounded number -> Maybe (Bounded number)
-tryDec bounded =
-    tryDecBy 1 bounded
-
-
-incBy : number -> Bounded number -> Bounded number
-incBy number bounded =
-    set (value bounded + number) bounded
-
-
-decBy : number -> Bounded number -> Bounded number
-decBy number bounded =
-    set (value bounded - number) bounded
-
-
-tryIncBy : number -> Bounded number -> Maybe (Bounded number)
-tryIncBy number bounded =
-    trySet (value bounded + number) bounded
-
-
-tryDecBy : number -> Bounded number -> Maybe (Bounded number)
-tryDecBy number bounded =
-    trySet (value bounded - number) bounded
 
 
 {-| Transforms a `Bounded` value with the given function. If the value returned by the given
@@ -171,5 +215,49 @@ min (Bounded { min_ }) =
 {-| Get the upper bound
 -}
 max : Bounded number -> number
-max (Bounded { min_ }) =
-    min_
+max (Bounded { max_ }) =
+    max_
+
+
+{-| Get a JSON decoder for bounded numbers. You specify a JSON decoder for either ints or floats
+so that this function knows which number type it should parse.
+
+The returned decoder expects that the JSON representation of a bounded number is a JSON object
+containing 3 number fields: "min", "max" and "value".
+
+When using int values the JSON might look like this:
+
+    { "min": 1
+    , "max": 15
+    , "value": 3
+    }
+
+The JSON value of a bounded float value might look like this:
+
+    { "min": 0
+    , "max": 1
+    , "value": 0.5
+    }
+
+-}
+decoder : JD.Decoder number -> JD.Decoder (Bounded number)
+decoder numberDecoder =
+    JD.map3 (\min_ max_ value_ -> Bounded { min_ = min_, max_ = max_, value_ = value_ })
+        (JD.field "min" numberDecoder)
+        (JD.field "max" numberDecoder)
+        (JD.field "value" numberDecoder)
+
+
+{-| Encode a bounded number as a JSON value. You specify a JSON encoder for either ints or floats
+so that this function knows which number type to use.
+
+The returned JSON value is a JSON object with 3 number fields: "min", "max" and "value".
+
+-}
+encode : (number -> JE.Value) -> Bounded number -> JE.Value
+encode numberEncoder (Bounded { min_, max_, value_ }) =
+    JE.object
+        [ ( "min", numberEncoder min_ )
+        , ( "max", numberEncoder max_ )
+        , ( "value", numberEncoder value_ )
+        ]
